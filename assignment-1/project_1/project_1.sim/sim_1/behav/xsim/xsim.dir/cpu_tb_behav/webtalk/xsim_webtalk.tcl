356e360538db8cea144c538a517193560e1dee47
15711657500 <15711657500@163.com>
2020-04-06 17:02:04 +0800

    update

--- assignment-1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/cpu_tb_behav/webtalk/xsim_webtalk.tcl
+++ assignment-1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/cpu_tb_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir /home/tanyifan/Desktop/ICS-Spring20-Fudan/assignment-1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/cpu_tb_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sun Mar 15 17:47:56 2020" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat Apr  4 19:27:35 2020" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.3 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2405991" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "LIN64" -context "software_version_and_target_device"
@@ -14,11 +14,11 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "092e16e7-cbcc-46c5-9df6-286653a0d0d3" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "7cfd47e1963542779af7e786a88e4a5b" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "13" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "93" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Ubuntu" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "Ubuntu 18.04.3 LTS" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-9750H CPU @ 2.60GHz" -context "user_environment"
-webtalk_add_data -client project -key cpu_speed -value "2592.005 MHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_speed -value "2591.998 MHz" -context "user_environment"
 webtalk_add_data -client project -key total_processors -value "4" -context "user_environment"
 webtalk_add_data -client project -key system_ram -value "6.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
@@ -27,7 +27,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "138740 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "2" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.04_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.06_sec" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Memory -value "137956_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 1934535025 -regid "" -xml /home/tanyifan/Desktop/ICS-Spring20-Fudan/assignment-1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/cpu_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/tanyifan/Desktop/ICS-Spring20-Fudan/assignment-1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/cpu_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/tanyifan/Desktop/ICS-Spring20-Fudan/assignment-1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/cpu_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_transmit -clientid 1906649339 -regid "" -xml /home/tanyifan/Desktop/ICS-Spring20-Fudan/assignment-1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/cpu_tb_behav/webtalk/usage_statistics_ext_xsim.xml -html /home/tanyifan/Desktop/ICS-Spring20-Fudan/assignment-1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/cpu_tb_behav/webtalk/usage_statistics_ext_xsim.html -wdm /home/tanyifan/Desktop/ICS-Spring20-Fudan/assignment-1/project_1/project_1.sim/sim_1/behav/xsim/xsim.dir/cpu_tb_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
